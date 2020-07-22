28f36aed86d848fdebf186c55369be551b7a63e8
kboysm <lishal12@naver.com>
2020-07-23 02:10:45 +0900

    웹 스크래핑 후 파일로 저장

--- scrape2.js
+++ scrape2.js
@@ -1,5 +1,9 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs')
+const writeStream = fs.createWriteStream('post.csv');
+
+
 
 request('https://hssm93.tistory.com/', (error, response, html) => {
     if (!error && response.statusCode == 200) {
@@ -13,7 +17,11 @@ request('https://hssm93.tistory.com/', (error, response, html) => {
             const link = $(el).find('a').attr('href');
             const date = $(el).find('.date').text().replace(/,/, ' ');
             // console.log(title);
-            console.log(title, link, date);
-        })
+            // console.log(title, link, date);
+
+            // Write To CSV
+            writeStream.write(`${title}, ${link}, ${date} \n`); //여기서는 파일로 만들었지만 실제 프로젝트에서는 DB에 넣을거 같음
+        });
+        console.log('Scraping Done...');
     }
 });
